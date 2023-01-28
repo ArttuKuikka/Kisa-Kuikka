318670f24df02247062c4ae63bf6ce839f54e7df
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-01-28 12:22:00 +0200

    Kaikki headerit toimii

--- Controllers/LatausController.cs
+++ Controllers/LatausController.cs
@@ -44,31 +44,53 @@ namespace Kipa_plus.Controllers
 
                 //etsi joka rasti ja laske niiden kaikkien tehtävien formitemeiden määrä ja laita se rastin headerin pituudeksi
                 IRow RastinNimetRow = sheet.CreateRow(0);
+                IRow RastinTehtävätRow = sheet.CreateRow(1);
+                IRow FormItemRow = sheet.CreateRow(2);
                 var lastindex = 1;
+                var RastinTehtävätRowlastindex = 1;
+                var FormItemRowlastindex = 1;
                 foreach (var rasti in _context.Rasti.Where(x => x.KisaId == Kisa.Id))
                 {
                     var SarjanTehtävätrastisssa = _context.Tehtava.Where(x => x.SarjaId == sarja.Id).Where(x => x.RastiId == rasti.Id);
                     int rastinpituus = 0;
-                    foreach(var item in SarjanTehtävätrastisssa)
+
+                    
+                    foreach (var item in SarjanTehtävätrastisssa)
                     {
                         if(item.TehtavaJson == null)
                         {
                             continue;
                         }
+                        var TehtäväNimiHeaderPituus = 0;
+
+
                         var tehtäväpohjat = JArray.Parse(item.TehtavaJson);
 
                         foreach(var tehtava in tehtäväpohjat)
                         {
                             rastinpituus++;
+                            TehtäväNimiHeaderPituus++;
+
+                            var formitemname = FormItemRow.CreateCell(FormItemRowlastindex);
+                            formitemname.SetCellValue(tehtava["label"].ToString());
+                            FormItemRowlastindex++;
                         }
+
+                        sheet.AddMergedRegion(new CellRangeAddress(1, 1, RastinTehtävätRowlastindex, RastinTehtävätRowlastindex + TehtäväNimiHeaderPituus - 1));
+                        var TehtäväNimiCell = RastinTehtävätRow.CreateCell(RastinTehtävätRowlastindex);
+                        TehtäväNimiCell.SetCellValue(item.Nimi);
+                        TehtäväNimiCell.CellStyle = centertext;
+
+                        RastinTehtävätRowlastindex = RastinTehtävätRowlastindex + TehtäväNimiHeaderPituus;
+
                     }
 
-                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, lastindex, lastindex + rastinpituus));
+                    sheet.AddMergedRegion(new CellRangeAddress(0, 0, lastindex, lastindex + rastinpituus - 1));
                     var RastinCell = RastinNimetRow.CreateCell(lastindex);
                     RastinCell.SetCellValue(rasti.Nimi);
                     RastinCell.CellStyle = centertext;
 
-                    lastindex = lastindex + rastinpituus +1;
+                    lastindex = lastindex + rastinpituus;
 
 
                 }
