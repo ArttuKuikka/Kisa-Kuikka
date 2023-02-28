019fb6b4bf67b2e1cf64d2d18ccdd67099d7463d
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-02-28 17:49:51 +0200

    JsonIgnoret subcontrolleriin ja actioniin

--- Models/DynamicAuth/Custom/Action.cs
+++ Models/DynamicAuth/Custom/Action.cs
@@ -1,9 +1,12 @@
-﻿namespace Kipa_plus.Models.DynamicAuth.Custom
+﻿using Newtonsoft.Json;
+
+namespace Kipa_plus.Models.DynamicAuth.Custom
 {
     public class Action
     {
         public string Name { get; set; }
 
+        [JsonIgnore]
         public string? DisplayName { get; set; }
         
     }
--- Models/DynamicAuth/Custom/SubController.cs
+++ Models/DynamicAuth/Custom/SubController.cs
@@ -1,8 +1,11 @@
-﻿namespace Kipa_plus.Models.DynamicAuth.Custom
+﻿using Newtonsoft.Json;
+
+namespace Kipa_plus.Models.DynamicAuth.Custom
 {
     public class SubController
     {
         public string Name { get; set; }
+        [JsonIgnore]
         public string? DisplayName { get; set; }
         public IEnumerable<Action> Actions { get; set; }
     }
