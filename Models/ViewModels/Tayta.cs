291ced541ee94292f1153488c7b7afab7b1a2756
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-03-01 11:05:33 +0200

    RastiId post requesteihin ja antiForgery vaihto

--- Models/ViewModels/Tayta.cs
+++ Models/ViewModels/Tayta.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore.Query;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kipa_plus.Models.ViewModels
 {
     public class Tayta
     {
         public int? TehtavaId { get; set; }
+        [NotMapped]
+        public int? RastiId { get; set; }
         public string? Nimi { get; set; }
         public int VartioId { get; set; }
         public bool Kesken { get; set; }
