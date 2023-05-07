5ee78f32057da9cda09f5619e42584795f620959
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-05-07 13:37:43 +0300

    tag skannaus parannuksia

--- Models/TagSkannaus.cs
+++ Models/TagSkannaus.cs
@@ -5,7 +5,6 @@
         public int? Id { get; set; }
         public int? VartioId { get; set; }
         public int RastiId { get; set; }
-        public string? TagSerial { get; set; }
         public DateTime TimeStamp { get; set; }
         public bool? isTulo { get; set; }
     }
--- Models/ViewModels/ManuaalinenTagSkannausViewModel.cs
+++ Models/ViewModels/ManuaalinenTagSkannausViewModel.cs
@@ -7,6 +7,7 @@ namespace Kisa_Kuikka.Models.ViewModels
     {
         public IQueryable<Vartio>? Vartiot { get; set; }
         public int RastiId { get; set; }
+        public string? RastiNimi { get; set; }
         public int ValittuVartioId { get; set; }
         [DisplayName("Aika")]
         [Required(ErrorMessage = "Tämä kenttä on pakollinen")]
