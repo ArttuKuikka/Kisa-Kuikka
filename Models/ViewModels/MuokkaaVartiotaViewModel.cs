6b0fad77b44765b603155c597fa3e9138cc7fa6e
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2023-06-15 19:59:48 +0300

    Vartio Edit parannus

---
+++ Models/ViewModels/MuokkaaVartiotaViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Kisa_Kuikka.Models.ViewModels
+{
+    public class MuokkaaVartiotaViewModel
+    {
+        public string Nimi { get; set; }
+        public int Numero { get; set; }
+        public int SarjaId { get; set; }
+        public int Id { get; set; }
+        public string? Lippukunta { get; set; }
+        public List<Sarja>? Sarjat { get; set; }
+        public string? olemassaOlevatVartiot { get; set; }
+    }
+}
