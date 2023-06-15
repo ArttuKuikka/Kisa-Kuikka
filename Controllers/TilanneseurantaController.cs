9bbb656f2d637a1464b267bdd3faadda65e66d20
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2023-06-15 20:58:27 +0300

    Tulosten seuranta alkua

--- Controllers/TilanneseurantaController.cs
+++ Controllers/TilanneseurantaController.cs
@@ -68,8 +68,8 @@ namespace Kisa_Kuikka.Controllers
 
 
 
-
-        [DisplayName("Etusivu (taulukko)")]
+        [HttpGet("/TilanneSeuranta")]
+        [DisplayName("Vartioiden seuranta(etusivu)")]
         public async Task<IActionResult> Index(int kisaId)
         {
             var kisa = await _context.Kisa.FindAsync(kisaId);
@@ -259,6 +259,205 @@ namespace Kisa_Kuikka.Controllers
                 }
 
                 
+
+                viewModel.Json = MainArray.ToString();
+                return View(viewModel);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet("/TilanneSeuranta/TulostenSyotto")]
+        [DisplayName("Tulosten syötön seuranta")]
+        public async Task<IActionResult> TulostenSyotto(int kisaId)
+        {
+            var kisa = await _context.Kisa.FindAsync(kisaId);
+            if (kisa != null)
+            {
+                var viewModel = new Models.ViewModels.TilanneseurantaTaulukkoViewModel() { Kisa = kisa };
+
+                //pää array
+                var MainArray = new JArray();
+
+                //sarjat ja rastit listat
+                var sarjat = _context.Sarja.Where(x => x.KisaId == kisaId).ToList();
+                var rastit = _context.Rasti.Where(x => x.KisaId == kisaId).Where(x => x.PiilotaTilanneseurannasta == false).ToList();
+                rastit.Sort((p1, p2) => p1.Numero.CompareTo(p2.Numero));
+
+                //rastien nimet
+                var RastiNimetArray = new JArray() { " " };
+
+                foreach (var rasti in rastit)
+                {
+                    RastiNimetArray.Add(rasti.Nimi);
+                }
+                viewModel.Headers = RastiNimetArray.ToString();
+
+
+
+                foreach (var sarja in sarjat)
+                {
+
+                    //rastien numero ja vapaat paikat
+                    bool sarjaRivi = true;
+                    var RastiNumeroArray = new JArray() { sarja.Nimi };
+
+                    foreach (var rasti in rastit)
+                    {
+                        if (rasti.tehtavaPaikat != null)
+                        {
+                            var rastilla = _context.TagSkannaus.Where(x => x.RastiId == rasti.Id).Where(x => x.isTulo == true)?.Count();
+                            RastiNumeroArray.Add($"{rasti.Numero} ({rastilla}/{rasti.tehtavaPaikat})");
+                        }
+                        else
+                        {
+                            RastiNumeroArray.Add(rasti.Numero);
+                        }
+
+                    }
+                    RastiNumeroArray.Add(sarjaRivi);
+                    MainArray.Add(RastiNumeroArray);
+
+
+
+                    var vartiotSarjassa = _context.Vartio.Where(x => x.KisaId == kisaId).Where(x => x.SarjaId == sarja.Id);
+                    //kaiken datan luonti
+                    foreach (var vartio in vartiotSarjassa)
+                    {
+                        var vartioobject = new JObject
+                        {
+                            { "Nimi", vartio.NumeroJaNimi },
+                            {"Keskeytetty", vartio.Keskeytetty },
+                            {"Lippukunta", vartio.Lippukunta }
+                        };
+                        var VartionArray = new JArray() { vartioobject };
+
+
+
+
+                        //seuraavan rastin tunnistus
+                        //luo lista json perusteella rastien järjestyksestä jossa ne kuuluisi mennä
+                        var uudetrastit = _context.Rasti.Where(x => x.KisaId == sarja.KisaId).Where(x => x.PiilotaTilanneseurannasta == false).ToList();
+
+                        var rastitJärjestyksessä = new List<Rasti>();
+                        if (sarja.RastienJarjestysJSON != null)
+                        {
+                            foreach (var Jrasti in JArray.Parse(sarja.RastienJarjestysJSON))
+                            {
+                                var success = int.TryParse(Jrasti["id"]?.ToString(), out var parsedid);
+                                if (success)
+                                {
+                                    var findrasti = await _context.Rasti.FindAsync(parsedid);
+                                    if (findrasti != null)
+                                    {
+                                        rastitJärjestyksessä.Add(findrasti);
+                                        uudetrastit.Remove(findrasti);
+                                    }
+                                }
+                            }
+                        }
+                        //jos puuttuu uusia rasteja lisää ne loppuun
+                        uudetrastit.ForEach(x => rastitJärjestyksessä.Add(x));
+
+
+
+                        //rastit jotka vartio on suorittanu 
+                        var suoritetutrastitSkannaukset = _context.TagSkannaus.Where(x => x.VartioId == vartio.Id).ToList();
+                        suoritetutrastitSkannaukset = suoritetutrastitSkannaukset.OrderByDescending(x => x.TimeStamp).ToList();
+                        var suoritetutRastit = new List<Rasti>();
+                        foreach (var suoritus in suoritetutrastitSkannaukset)
+                        {
+                            var findrasti = await _context.Rasti.FindAsync(suoritus.RastiId);
+                            if (findrasti != null)
+                            {
+                                suoritetutRastit.Add(findrasti);
+                            }
+                        }
+                        suoritetutRastit.Reverse();
+
+                        Rasti? seuraavaRasti = null;
+                        //poista duplicatet
+                        suoritetutRastit = suoritetutRastit.Distinct().ToList();
+                        var eka = suoritetutRastit.FirstOrDefault();
+                        if (eka != null)
+                        {
+                            var ekaindex = rastitJärjestyksessä.IndexOf(eka);
+
+                            var lista = new List<Rasti>();
+                            for (int i = ekaindex; i < suoritetutRastit.Count; i++)
+                            {
+                                lista.Add(rastitJärjestyksessä[i]);
+                            }
+                            //tarkista onko järjestys sama
+                            if (lista.SequenceEqual(suoritetutRastit))
+                            {
+                                var pos = ekaindex + suoritetutRastit.Count;
+                                if (rastitJärjestyksessä.ElementAtOrDefault(pos) != null)
+                                {
+                                    seuraavaRasti = rastitJärjestyksessä[pos];
+                                }
+                            }
+
+                        }
+
+
+
+
+
+
+                        foreach (var rasti in rastit)
+                        {
+                            var dataelement = new JObject();
+                            var skannaukset = _context.TagSkannaus.Where(x => x.RastiId == rasti.Id)?.Where(x => x.VartioId == vartio.Id);
+                            if (skannaukset != null)
+                            {
+
+                                if (seuraavaRasti != null && seuraavaRasti == rasti)
+                                {
+                                    dataelement.Add("Numero", 4);
+                                }
+                                else
+                                {
+                                    //0 - ei mitään
+                                    //1 - pelkkä lähtö
+                                    //2 - pelkkä tulo
+                                    //3 - lähtö ja tulo
+                                    //4 - seuraava rasti
+                                    var tulo = skannaukset.Where(x => x.isTulo == true).FirstOrDefault();
+                                    var lähtö = skannaukset.Where(x => x.isTulo == false).FirstOrDefault();
+                                    if (tulo != null && lähtö != null)
+                                    {
+                                        dataelement.Add("Numero", 3);
+                                    }
+                                    else if (tulo != null)
+                                    {
+                                        dataelement.Add("Numero", 2);
+                                    }
+                                    else if (lähtö != null)
+                                    {
+                                        dataelement.Add("Numero", 1);
+                                    }
+                                    else
+                                    {
+                                        dataelement.Add("Numero", 0);
+                                    }
+                                    dataelement.Add("Tulo", tulo?.TimeStamp);
+                                    dataelement.Add("Lahto", lähtö?.TimeStamp);
+                                }
+
+
+                            }
+                            else
+                            {
+                                dataelement.Add("Numero", 0);
+                            }
+                            VartionArray.Add(dataelement);
+                        }
+                        MainArray.Add(VartionArray);
+                    }
+                }
+
+
 
                 viewModel.Json = MainArray.ToString();
                 return View(viewModel);
